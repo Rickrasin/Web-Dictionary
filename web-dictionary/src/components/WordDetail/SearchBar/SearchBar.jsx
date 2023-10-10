eac4c78e5499053f697b77fafeb3e914f24a1637
Rickson de Oliveira Viana <rickson2013oliveira@gmail.com>
2023-10-09 21:57:42 -0300

    Fix SelectFont.css, added search button in Searchbar

--- web-dictionary/src/components/WordDetail/SearchBar/SearchBar.jsx
+++ web-dictionary/src/components/WordDetail/SearchBar/SearchBar.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { motion } from "framer-motion";
+import searchIcon from "./../../../assets/images/icon-search.svg";
+import { motion, useAnimation } from "framer-motion";
 import "./SearchBar.css";
 
 import { setWordData } from "../../../redux/API/apiReducer";
-//import { faL } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [wordEmpty, setWordEmpty] = useState(false);
-
+  const controls = useAnimation();
   const dispatch = useDispatch();
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value.trim());
+  };
+
+  const handleClick = () => {
+    // Iniciar a animação quando o botão for clicado
+    controls.start({ scale: 1, opacity: 0.1 });
+
+    // Definir um timeout para reverter a animação após um período (por exemplo, 1 segundo)
+    setTimeout(() => {
+      controls.start({ scale: 0, opacity: 0 });
+    }, 340);
   };
 
   const handleSubmit = async (e) => {
@@ -43,10 +53,25 @@ const SearchBar = () => {
           value={searchTerm}
           onChange={handleSearch}
         />
+
+        <button
+          className="sb-search-btn"
+          onClick={handleClick}
+          onSubmit={handleSearch}
+          type="submit"
+        >
+          <img src={searchIcon} />
+          <motion.div
+            className="sb-circle"
+            initial={{ scale: 0 }}
+            animate={controls}
+          />
+        </button>
+
         {wordEmpty && (
           // eslint-disable-next-line react/no-unescaped-entities
           <motion.div animate={{ opacity: [0, 0.5, 1], scaleY: [0.6, 1] }}>
-            <p className="sb-error-text">Whoops, can't be empty...</p>
+            <p className="sb-error-text">Whoops, can&lsquo;t be empty...</p>
           </motion.div>
         )}
       </form>
